580a1ad5e1609dfb6affd82cfa0c26b76362a506
Amirul Zafrin <amirulzafrin98@gmail.com>
2022-07-25 20:23:34 +0800

    Fixed flake8 issues

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,11 @@
 To run the code, write in the terminal:
 uvicorn main:app --reload
 """
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
