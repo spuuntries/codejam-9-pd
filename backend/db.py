40f28f22ed8a17e9962cbcd180bf8bdf1d8e2a1b
spuun <kek@spuun.art>
2022-07-31 22:01:31 +0700

    WS handling, DB handling

--- backend/db.py
+++ backend/db.py
@@ -2,73 +2,29 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from uuid import uuid4
-from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from tinydb import TinyDB, Query
 import datetime
-import sqlalchemy
 import os
 
 if not os.path.isdir("./data"):
     os.makedirs("./data")
 if not os.path.isdir("./documents"):
     os.makedirs("./documents")
-metadata = sqlalchemy.MetaData()
-Base = declarative_base()
-
-
-class user_data(Base):
-    __tablename__ = "user_data"
-
-    id = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
-    username = sqlalchemy.Column(sqlalchemy.String)
-    password = sqlalchemy.Column(sqlalchemy.String)
-    salt = sqlalchemy.Column(sqlalchemy.String)
-    creation_date = sqlalchemy.Column(sqlalchemy.Date)
-    char_count = sqlalchemy.Column(sqlalchemy.ARRAY(sqlalchemy.Integer, dimensions=1))
-    score = sqlalchemy.Column(sqlalchemy.Float)
-
-    documents_id = sqlalchemy.Column(
-        sqlalchemy.String, sqlalchemy.ForeignKey("documents.id")
-    )
-    documents = relationship("documents", back_populates="user_data", uselist=True)
-
-
-class documents(Base):
-    __tablename__ = "documents"
-
-    id = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
-    name = sqlalchemy.Column(sqlalchemy.String)
-    path = sqlalchemy.Column(sqlalchemy.String)
-    creation_date = sqlalchemy.Column(sqlalchemy.Date)
-
-    owner_id = sqlalchemy.Column(
-        sqlalchemy.String, sqlalchemy.ForeignKey("user_data.id")
-    )
-    owner = relationship("user_data", back_populates="documents", uselist=False)
-
-
-class generated_ids(Base):
-    __tablename__ = "generated_ids"
-
-    ids = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
-
-
-engine = sqlalchemy.create_engine(
-    f"sqlite:///./data/store.db", connect_args={"check_same_thread": False}
-)
-metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
 
 
 class DatabaseHandler:
     def __init__(self):
-        self.generated_ids = []
+        self.db = TinyDB("./data/db.json")
+        self.generated_ids = self.db.table("generated_ids")
+        self.users = self.db.table("users")
+        self.documents = self.db.table("documents")
 
     async def generate_id(self):
-        new_id = uuid4()
-        while new_id in self.generated_ids:
+        new_id = uuid4().__str__()
+        while self.generated_ids.contains(Query()["id"] == new_id.__str__()):
             new_id = uuid4()
-        self.generated_ids.append(new_id)
+        self.generated_ids.insert({"id": new_id.__str__()})
+        self.db.close()
         return new_id
 
     async def create_user(self, username: str, password: str, salt: str):
@@ -82,18 +38,18 @@ class DatabaseHandler:
         user_id = await self.generate_id()
         creation_date = datetime.datetime.now()
 
-        new_user = user_data(
-            id=user_id,
-            username=username,
-            password=password,
-            salt=salt,
-            creation_date=creation_date,
-            char_count=[10] * 36,
-            score=0,
-        )
+        new_user = {
+            "id": user_id,
+            "username": username,
+            "password": password,
+            "salt": salt,
+            "creation_date": creation_date,
+            "char_count": [10] * 62,
+            "score": 0,
+        }
 
-        session.add(new_user)
-        session.commit()
+        self.users.insert(new_user)
+        self.db.close()
         return new_user
 
     async def create_document(self, user_id: str, doc_name: str) -> bool:
@@ -101,8 +57,8 @@ class DatabaseHandler:
             raise AttributeError(
                 '"doc_name" parameter not found when creating document!'
             )
-        check_user = session.query(user_data).filter(user_data.id == user_id).count()
-        if check_user < 1:
+        check_user = self.users.contains(Query()["id"] == user_id)
+        if not check_user:
             raise ValueError('"user_id" not found in database!')
 
         doc_id = await self.generate_id()
@@ -111,14 +67,93 @@ class DatabaseHandler:
         # Create the file
         open(f"./documents/{doc_id}-{doc_name}-{creation_date.isoformat()}.md", "w")
 
-        new_doc = documents(
-            id=doc_id,
-            owner_id=user_id,
-            name=doc_name,
-            path=f"./documents/{doc_id}-{doc_name}-{creation_date.isoformat()}.md",
-            creation_date=creation_date,
-        )
+        new_doc = {
+            "id": doc_id,
+            "name": doc_name,
+            "creation_date": creation_date,
+            "owner_id": user_id,
+            "path": f"./documents/{doc_id}-{doc_name}-{creation_date.isoformat()}.md",
+        }
 
-        session.add(new_doc)
-        session.commit()
+        self.documents.insert(new_doc)
+        self.db.close()
         return new_doc
+
+    async def get_user(self, user_id: str) -> dict:
+        check_user = self.users.contains(Query()["id"] == user_id)
+        if not check_user:
+            raise ValueError('"user_id" not found in database!')
+        self.db.close()
+        return self.users.search(Query()["id"] == user_id)[0]
+
+    async def get_document(self, doc_id: str) -> dict:
+        check_doc = self.documents.contains(Query()["id"] == doc_id)
+        if not check_doc:
+            raise ValueError('"doc_id" not found in database!')
+        self.db.close()
+        return self.documents.search(Query()["id"] == doc_id)[0]
+
+    async def update_char_count(self, user_id: str, char_count: list):
+        check_user = self.users.contains(Query()["id"] == user_id)
+        if not check_user:
+            raise ValueError('"user_id" not found in database!')
+        self.users.update({"char_count": char_count}, Query()["id"] == user_id)
+        self.db.close()
+        
+    async def update_score(self, user_id: str, score: int):
+        check_user = self.users.contains(Query()["id"] == user_id)
+        if not check_user:
+            raise ValueError('"user_id" not found in database!')
+        self.users.update({"score": score}, Query()["id"] == user_id)
+        self.db.close()
+
+    async def get_user_documents(self, user_id: str) -> list:
+        check_user = self.users.contains(Query()["id"] == user_id)
+        if not check_user:
+            raise ValueError('"user_id" not found in database!')
+        return self.documents.search(Query()["owner_id"] == user_id)
+
+    async def update_document(self, doc_id: str, doc_name: str):
+        check_doc = self.documents.contains(Query()["id"] == doc_id)
+        if not check_doc:
+            raise ValueError('"doc_id" not found in database!')
+        self.documents.update({"name": doc_name}, Query()["id"] == doc_id)
+        self.db.close()
+
+    async def delete_document(self, doc_id: str):
+        check_doc = self.documents.contains(Query()["id"] == doc_id)
+        if not check_doc:
+            raise ValueError('"doc_id" not found in database!')
+        os.remove(self.documents.search(Query()["id"] == doc_id)[0]["path"])
+        self.documents.remove(Query()["id"] == doc_id)
+        self.db.close()
+
+    async def delete_user(self, user_id: str):
+        check_user = self.users.contains(Query()["id"] == user_id)
+        if not check_user:
+            raise ValueError('"user_id" not found in database!')
+        self.users.remove(Query()["id"] == user_id)
+        self.db.close()
+
+    async def write_document(self, doc_id: str, text: str):
+        check_doc = self.documents.contains(Query()["id"] == doc_id)
+        if not check_doc:
+            raise ValueError('"doc_id" not found in database!')
+        with open(self.documents.search(Query()["id"] == doc_id)[0]["path"], "a") as f:
+            # Append the text to the file
+            f.write(text)
+        file_len = open(self.documents.search(Query()["id"] == doc_id)[0]["path"], "r").read().__len__()
+        return file_len
+
+    async def read_document(self, doc_id: str) -> str:
+        check_doc = self.documents.contains(Query()["id"] == doc_id)
+        if not check_doc:
+            raise ValueError('"doc_id" not found in database!')
+        with open(self.documents.search(Query()["id"] == doc_id)[0]["path"], "r") as f:
+            return f.read()
+    
+    async def get_char_count(self, user_id: str) -> list:
+        check_user = self.users.contains(Query()["id"] == user_id)
+        if not check_user:
+            raise ValueError('"user_id" not found in database!')
+        return self.users.search(Query()["id"] == user_id)[0]["char_count"]
--- backend/main.py
+++ backend/main.py
@@ -2,21 +2,184 @@
 To run the code, write in the terminal:
 uvicorn main:app --reload
 """
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, status
+from db import DatabaseHandler
 
 app = FastAPI()
-
+db = DatabaseHandler()
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
 @app.websocket("/ws")
-async def ws_end(ws: WebSocket, route):
+async def ws_end(ws: WebSocket, route: str):
     await ws.accept()
-    match route:
-        case 1:
-            while True:
-                data = await ws.receive_json()
-                
-                
+    if not route:
+        await ws.close(code=status.WS_1002_PROTOCOL_ERROR)
+        return
+    while True:
+        json = await ws.receive_json()
+        if not json:
+            await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+            return
+        match route:
+            case "get_user_documents":
+                if not json["user_id"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    documents = await db.get_user_documents(json["user_id"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send(documents)
+
+            case "update_score":
+                if not json["user_id"] or not json["score"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    await db.update_score(json["user_id"], json["score"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send("Success!")
+
+            case "get_user":
+                if not json["user_id"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    user = await db.get_user(json["user_id"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send(user)
+
+            case "get_document":
+                if not json["doc_id"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    document = await db.get_document(json["doc_id"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send(document)
+
+            case "update_document":
+                if not json["doc_id"] or not json["doc_name"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    await db.update_document(json["doc_id"], json["doc_name"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send("Success!")
+
+            case "delete_document":
+                if not json["doc_id"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    await db.delete_document(json["doc_id"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send("Success!")
+
+            case "delete_user":
+                if not json["user_id"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    await db.delete_user(json["user_id"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send("Success!")
+            case "create_user":
+                if not json["user_id"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    await db.create_user(json["user_id"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send("Success!")
+
+            case "create_document":
+                if not json["doc_name"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    await db.create_document(json["doc_name"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send("Success!")
+
+            case "write_document":
+                if not json["doc_id"] or not json["char"] or not json["user_id"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    await db.write_document(json["doc_id"], json["char"])
+                    anum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+                    current_char_count = await db.get_char_count(json["user_id"])
+                    index = anum.index(json["char"])
+                    current_char_count = current_char_count[index] - 1
+                    await db.update_char_count(json["user_id"], current_char_count)
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send("Success!")
+
+            case "read_document":
+                if not json["doc_id"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    document = await db.read_document(json["doc_id"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send(document)
+
+            case "get_char_count":
+                if not json["user_id"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    char_count = await db.get_char_count(json["user_id"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send(char_count)
+
+            case "get_score":
+                if not json["user_id"]:
+                    await ws.close(code=status.WS_1008_POLICY_VIOLATION)
+                    return
+                try:
+                    score = await db.get_score(json["user_id"])
+                except ValueError as e:
+                    await ws.send(e.args[0])
+                    await ws.close()
+                    return
+                await ws.send(score)
